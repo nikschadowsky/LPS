702b68ac72870984a75d7dfa878183dae1e1dafa
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-07 14:33:11 +0100

    use lps scanner

--- src/antenna/main.cpp
+++ src/antenna/main.cpp
@@ -3,23 +3,29 @@
 #include "LPSDevice.h"
 #include "LPSScanner.h"
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 
   Serial.begin(115200);
   Serial.println("Starting");
-  // initialize scanner
-  init("");
-  std::vector<LPSDEVICE> devices = scan();
 
-  for(LPSDEVICE device : devices) {
+  // initialize scanner
+  LPSSCANNER::init("");
+  std::vector<LPSDEVICE> devices = LPSSCANNER::scan();
+
+  for (LPSDEVICE device : devices)
+  {
     Serial.println(getDeviceFormatted(device).c_str());
   }
 
   Serial.println("\nDone!");
+
+  devices.clear();
+
+  LPSSCANNER::deinit();
 }
 
-
-
-void loop() {
+void loop()
+{
 }
