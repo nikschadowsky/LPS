4fb2e3737f00aed563fddf110ab5b99cdcfc375f
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-08 14:08:48 +0100

    add secrets header

---
+++ include/LPSSecret.h
@@ -0,0 +1,12 @@
+#ifndef LPSSECRET_H
+#define LPSSECRET_H
+
+#include <string>
+
+class LPSSecret
+{
+    static const std::string LPS_SSID;
+    static const std::string LPS_PASSCODE;
+};
+
+#endif
