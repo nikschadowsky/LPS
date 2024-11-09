b5a90ff5cee86976c46900f4234e3c28ecadb526
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-09 12:09:50 +0100

    add describing documentation

--- include/LPSSecret.h
+++ include/LPSSecret.h
@@ -3,7 +3,14 @@
 
 #include <string>
 
+/**
+ *  Defines the SSID of the WiFi network the antennas connect to. An associated CPP file has to be created and filled manually!
+ */
 extern const std::string LPS_SSID;
+
+/**
+ * Defines the passcode of the WiFi network the antennas connect to. An associated CPP file has to be created and filled manually!
+ */
 extern const std::string LPS_PASSCODE;
 
 #endif
