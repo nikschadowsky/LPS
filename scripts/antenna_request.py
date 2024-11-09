1e8bf1506cb57ae7c97afe386d5da337bcfdb6b6
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-09 12:24:00 +0100

    update http request script to include header information

--- scripts/antenna_request.py
+++ scripts/antenna_request.py
@@ -1,7 +1,13 @@
+# Test script for running HTTP requests.
+#
+# This script requests an URL to HTTP GET from, 
+# printing the resulting headers and body to the console.
+
 import requests;
 
 print('Request from: ')
 response = requests.get(input())
 
-print(f'Response status: {response.status_code}')
-print(f'Response received: {response.content}')
+print(f'Status: {response.status_code}')
+print(f'Response header: {response.headers}')
+print(f'Response body: {response.content}')
