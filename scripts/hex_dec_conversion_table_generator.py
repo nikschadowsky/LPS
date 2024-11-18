78c6b01908bfad4d5a100bed673070d6cb0d82b8
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-18 17:09:54 +0100

    update gitignore

--- scripts/hex_dec_conversion_table_generator.py
+++ scripts/hex_dec_conversion_table_generator.py
@@ -1,5 +1,7 @@
 ROW_WIDTH = 8
 
+file = open("./temp/binary_translation_matrix.txt", "x")
+
 lines = []
 
 # lines.append(["Hex", "Binary", "Dec", "u-Dec"])
@@ -12,8 +14,11 @@ for i in range(0, 256):
         f"{i:d}" # unsigned dec
         ])
     
-print(f'{"Hex".rjust(8)} | {"Binary".rjust(8)} | {"Dec".rjust(8)} | {"u-Dec".rjust(8)}')
-print('-' * 41)
+file.write(f'{"Hex".rjust(8)} | {"Binary".rjust(8)} | {"Dec".rjust(8)} | {"u-Dec".rjust(8)}')
+file.write('-' * 41)
+
 
 for line in lines:
-    print(f'{line[0].rjust(8)} | {line[1].rjust(8)} | {line[2].rjust(8)} | {line[3].rjust(8)}')
+    file.write(f'{line[0].rjust(8)} | {line[1].rjust(8)} | {line[2].rjust(8)} | {line[3].rjust(8)}')
+
+file.close()
