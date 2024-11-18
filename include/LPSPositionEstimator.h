3a8e470f6b5a76eae6219b87d29ee066a2c01028
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-18 22:38:19 +0100

    create first functions for position estimation

--- include/LPSPositionEstimator.h
+++ include/LPSPositionEstimator.h
@@ -1,4 +1,15 @@
 #ifndef LPS_POSITION_ESTIMATOR_H
 #define LPS_POSITION_ESTIMATOR_H
 
+#include "LPSDevice.h"
+
+typedef struct
+{
+    float x;
+    float y;
+    float uncertainty;
+} LPSPosition;
+
+float estimate_distance(const LPSDEVICE *device_ptr);
+
 #endif
--- src/controller/LPSPositionEstimator.cpp
+++ src/controller/LPSPositionEstimator.cpp
@@ -1,1 +1,13 @@
 #include "LPSPositionEstimator.h"
+#include <stdint.h>
+
+// reference RSSI at a distance of 1m from an antenna
+const int16_t REFERENCE_RSSI = -50;
+
+// worst case scenario. has to be dialed in by testing.
+const float ENVIRONMENTAL_PATH_LOSS = 4.0;
+
+float estimate_distance(const LPSDEVICE *device_ptr)
+{
+    return 0.0f;
+}
