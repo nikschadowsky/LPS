c3e7391714058949df91ddb288205a8f2c2c0adf
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-18 15:57:35 +0100

    WIP: implement number formatting

--- scripts/hex_dec_conversion_table_generator.py
+++ scripts/hex_dec_conversion_table_generator.py
@@ -2,4 +2,13 @@ lines = []
 
 lines.append(["Hex", "Binary", "Dec", "u-Dec"])
 
-for i in range(0,255):
+for i in range(0, 256):
+    lines.append([
+        f"{i:X}", # hex
+        f"{i:b}", # bin
+        f"{i if i < 128 else i - 256}", # signed dec
+        f"{i:d}" # unsigned dec
+        ])
+    
+for line in lines:
+    print(line)
