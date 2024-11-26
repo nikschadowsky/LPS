dcfeecee89e81c0ac4b5380a35eea7486c53fcf8
nikschadowsky <n.s-schadowsky@t-online.de>
2024-11-26 22:19:27 +0100

    add class for service serving the web socket

---
+++ src/main/java/lpsvisualizer/service/PositionWebSocketService.java
@@ -0,0 +1,20 @@
+package lpsvisualizer.service;
+
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @since 26.11.2024
+ */
+@Service
+public class PositionWebSocketService {
+
+    public PositionWebSocketService() {
+
+    }
+
+    public void sendPositions() {
+
+    }
+
+}
