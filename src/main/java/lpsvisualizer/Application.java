a0ce18cd4f3e6fc3c7f8be257451cd90b8aa5561
nikschadowsky <n.s-schadowsky@t-online.de>
2024-12-01 22:38:56 +0100

    implement starting of serial communicator

--- src/main/java/lpsvisualizer/Application.java
+++ src/main/java/lpsvisualizer/Application.java
@@ -1,5 +1,8 @@
 package lpsvisualizer;
 
+import lpsvisualizer.service.SerialCommunicator;
+import lpsvisualizer.websocket.PositionWebSocketHandler;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 25.11.2024
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    private SerialCommunicator serialCommunicator;
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        serialCommunicator = new SerialCommunicator(new PositionWebSocketHandler());
+        serialCommunicator.start();
     }
 }
