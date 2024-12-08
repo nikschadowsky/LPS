5b7c3b971da182f07260deaad424876776fe91ca
nikschadowsky <n.s-schadowsky@t-online.de>
2024-12-08 22:45:56 +0100

    remove unstable multitasking for a single task solution using a custom tcp socket

--- src/main/esp32/controller/LPSController.cpp
+++ src/main/esp32/controller/LPSController.cpp
@@ -14,13 +14,13 @@ IPAddress local_ip(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-struct HttpSubTaskData
+struct TCPSocketData
 {
     Antenna *antenna;
     std::vector<LPSDEVICE> *device_buffer;
 };
 
-LPSRoom room;
+LPSRoom *room_ptr;
 
 void setupWiFi()
 {
@@ -80,6 +80,8 @@ void setupLPSRoom()
         handle_http_toggle_config_mode(get_ip_as_string(netif_sta_list.sta[i].ip.addr), 1);
     }
 
+    room_ptr = new LPSRoom;
+
     Serial.write("ESP_CONFIG_START");
 
     // all antennas connected
@@ -92,96 +94,172 @@ void setupLPSRoom()
         // expecting A - D from visualizer
         Serial.write("ESP_CONFIG_REQ");
         while (Serial.available() < 1)
-        {
-        };
+            ;
 
         uint8_t response = Serial.read();
 
         // subtracting 65 since the requested data is ASCII A - D
         // position will be set later
         // for now we just associate the ip with one of the corners
-        room.corner[response - 65] = Antenna{Point{0, 0}, ip};
+        room_ptr->corner[response - 65] = Antenna{Point{0, 0}, ip};
 
         handle_http_toggle_config_mode(ip, 1);
     }
 
     // now setting the distances...
-    handle_http_toggle_config_mode(room.corner[0].ip, 0);
-    handle_http_toggle_config_mode(room.corner[1].ip, 0);
+    handle_http_toggle_config_mode(room_ptr->corner[0].ip, 0);
+    handle_http_toggle_config_mode(room_ptr->corner[1].ip, 0);
 
     uint8_t buffer[4];
     Serial.write("ESP_CONFIG_DIST1");
     while (Serial.available() < 4)
-    {
-    };
+        ;
+
     Serial.readBytes(buffer, 4);
 
     // expecting float
     float dist_ab = *(float *)buffer;
-    handle_http_toggle_config_mode(room.corner[1].ip, 1);
-    handle_http_toggle_config_mode(room.corner[3].ip, 0);
+    handle_http_toggle_config_mode(room_ptr->corner[1].ip, 1);
+    handle_http_toggle_config_mode(room_ptr->corner[3].ip, 0);
 
     Serial.write("ESP_CONFIG_DIST2");
     while (Serial.available() < 4)
-    {
-    };
+        ;
+
     Serial.readBytes(buffer, 4);
 
     float dist_ad = *(float *)buffer;
-    room.corner[1].position.x = dist_ab;
-    room.corner[2].position.x = dist_ab;
-    room.corner[2].position.y = dist_ad;
-    room.corner[3].position.y = dist_ad;
+    room_ptr->corner[1].position.x = dist_ab;
+    room_ptr->corner[2].position.x = dist_ab;
+    room_ptr->corner[2].position.y = dist_ad;
+    room_ptr->corner[3].position.y = dist_ad;
 
-    handle_http_toggle_config_mode(room.corner[0].ip, 1);
-    handle_http_toggle_config_mode(room.corner[3].ip, 1);
+    handle_http_toggle_config_mode(room_ptr->corner[0].ip, 1);
+    handle_http_toggle_config_mode(room_ptr->corner[3].ip, 1);
 }
 
-void handle_http_scan(const HttpSubTaskData *parameter_ptr)
+uint8_t HEADER_NAME_CONTENT_LENGTH_FIELD[] = "Content-Length";
+
+uint16_t get_content_length(const uint8_t *buffer, uint16_t body_offset)
 {
-    std::string url = "http://";
-    url.append(parameter_ptr->antenna->ip);
-    url.append(":");
-    url.append(std::to_string(LPS_ANTENNA_PORT));
-    url.append("/api/scan");
+    uint16_t content_length_field_offset = -1;
+    uint16_t field_name_length = 14;
 
-    HTTPClient client;
-    client.begin(url.c_str());
-
-    uint8_t response_code = client.GET();
-
-    if (response_code != 200)
+    for (uint16_t i = 0; i < body_offset - 14; i++)
     {
-        client.end();
-        return;
+        if (memcmp(buffer + i, HEADER_NAME_CONTENT_LENGTH_FIELD, 14) == 0)
+        {
+            content_length_field_offset = i + 15;
+            break;
+        }
     }
 
-    uint16_t payload_length = client.getSize();
-    std::vector<uint8_t> payload;
-    payload.reserve(payload_length);
-
-    WiFiClient *stream = client.getStreamPtr();
-
-    while (stream->available())
-    {
-        payload.push_back(stream->read());
-    }
-
-    deserialize_devices(payload.data(), parameter_ptr->device_buffer);
-    client.end();
+    return atoi((char *)(buffer + content_length_field_offset));
 }
 
-void handle_http_task(void *task_parameter)
+void handle_tcp_socket(const TCPSocketData *antenna_data[TOTAL_NUMBER_OF_ANTENNAS])
 {
-    HttpSubTaskData *task_data_ptr = (HttpSubTaskData *)task_parameter;
+    uint32_t start_time;
 
-    // cleanup data we dont need
-    task_data_ptr->device_buffer->clear();
+    WiFiClient clients[TOTAL_NUMBER_OF_ANTENNAS];
 
-    handle_http_scan(task_data_ptr);
+    for (uint8_t i = 0; i < TOTAL_NUMBER_OF_ANTENNAS; i++)
+    {
+        if (clients[i].connect(antenna_data[i]->antenna->ip.c_str(), LPS_ANTENNA_PORT))
+        {
+            clients[i].print("GET /api/scan HTTP/1.1\r\n");
+            clients[i].print("Host:");
+            clients[i].print(antenna_data[i]->antenna->ip.c_str());
+            clients[i].print(":");
+            clients[i].print(std::to_string(LPS_ANTENNA_PORT).c_str());
+            clients[i].print("\r\n");
 
-    // each task self deletes after it finishes execution
-    vTaskDelete(NULL);
+            clients[i].print("Connection:close\r\n");
+            clients[i].print("\r\n");
+            clients[i].flush(); // sends request
+        }
+        else
+        {
+            // todo send prefix to serial client to inform them
+            Serial.println("Antenna unreachable: " + (i + 65));
+        }
+
+        start_time = millis();
+    }
+
+    for (uint8_t i = 0; i < TOTAL_NUMBER_OF_ANTENNAS; i++)
+    {
+        // wait for response...
+        if (clients[i].connected())
+        {
+            uint32_t current_time;
+
+            while (!clients[i].available() && (current_time = millis()) - start_time <= REQUEST_TIMEOUT_MILLIS)
+            {
+                vTaskDelay(50 / portTICK_PERIOD_MS);
+            }
+
+            if (current_time - start_time <= REQUEST_TIMEOUT_MILLIS)
+            {
+
+                uint16_t content_size = clients[i].available();
+                uint8_t *response_buffer = new uint8_t[content_size];
+
+                clients[i].readBytes(response_buffer, content_size);
+
+                uint16_t body_offset;
+
+                for (uint16_t j = 0; j < content_size - 3; j++)
+                {
+                    if (response_buffer[j] == 0x0D && response_buffer[j + 1] == 0x0A && response_buffer[j + 2] == 0x0D && response_buffer[j + 3] == 0x0A)
+                    {
+                        body_offset = j + 4;
+                        break;
+                    }
+                }
+
+                uint16_t content_length = get_content_length(response_buffer, body_offset);
+
+                uint8_t *body_buffer = new uint8_t[content_length];
+
+                if (body_offset == content_size)
+                {
+                    // body not available yet
+                    while (clients[i].available() < content_length)
+                        ;
+                    clients[i].readBytes(body_buffer, content_length);
+                }
+                else
+                {
+                    memcpy(body_buffer, response_buffer + body_offset, content_length);
+                }
+
+                deserialize_devices(body_buffer, antenna_data[i]->device_buffer);
+                delete[] (response_buffer);
+                delete[] (body_buffer);
+            }
+            else
+            {
+                // todo debug message to visualizer?
+                Serial.println("Request timeout on antenna: " + i);
+            }
+        }
+
+        clients[i].stop();
+    }
+}
+
+const TCPSocketData *tcp_socket_data_ptr[4]; // array of pointers to the data containers
+
+const TCPSocketData *tcp_socket_data_create(Antenna *antenna)
+{
+    std::vector<LPSDEVICE> *antenna_devices = new std::vector<LPSDEVICE>();
+
+    TCPSocketData *parameter = new TCPSocketData;
+    parameter->antenna = antenna;
+    parameter->device_buffer = antenna_devices;
+
+    return parameter;
 }
 
 void setup()
@@ -189,81 +267,27 @@ void setup()
     Serial.begin(115200);
     setupWiFi();
     setupLPSRoom();
+
+    for (uint8_t i = 0; i < TOTAL_NUMBER_OF_ANTENNAS; i++)
+    {
+        tcp_socket_data_ptr[i] = tcp_socket_data_create(&room_ptr->corner[i]);
+    }
 }
-
-const HttpSubTaskData *task_parameter_create(Antenna *antenna)
-{
-    std::vector<LPSDEVICE> *antenna_devices = new std::vector<LPSDEVICE>();
-
-    HttpSubTaskData *parameter = new HttpSubTaskData;
-    parameter->antenna = antenna;
-    parameter->device_buffer = antenna_devices;
-
-    return parameter;
-}
-
-const uint8_t NUM_SUB_TASKS = 4;
-
-TaskHandle_t http_task_handles[NUM_SUB_TASKS];
-
-const HttpSubTaskData *sub_task_data_ptr[NUM_SUB_TASKS] = {
-    task_parameter_create(&room.corner[0]),
-    task_parameter_create(&room.corner[1]),
-    task_parameter_create(&room.corner[2]),
-    task_parameter_create(&room.corner[3])};
-
-const std::string task_names[NUM_SUB_TASKS] = {"antenna_A_GET", "antenna_B_GET", "antenna_C_GET", "antenna_D_GET"};
 
 void loop()
 {
-    /*uint32_t timestamp = millis();
-
-    // create tasks
-    for (int i = 0; i < NUM_SUB_TASKS; i++)
+    for (auto data : tcp_socket_data_ptr)
     {
-        xTaskCreate(handle_http_task, task_names[i].c_str(), 4096, (void *)&sub_task_data_ptr[i], 0, &http_task_handles[i]);
+        data->device_buffer->clear();
     }
 
-    // since each scan task is going to take at least one second we can wait here.
-    vTaskDelay(900 / portTICK_PERIOD_MS);
-
-    while (true)
-    {
-        // wait out all tasks or the timeout period
-        if (millis() - timestamp > REQUEST_TIMEOUT_MILLIS)
-        {
-            Serial.println("Some tasks did not complete in time! Terminating...");
-
-            // if timeout period is exceeded, terminate tasks using their handle
-            for (auto handle : http_task_handles)
-            {
-                if (eTaskGetState(handle) != eDeleted)
-                {
-                    vTaskDelete(handle);
-                }
-            }
-            break;
-        }
-
-        // if all tasks terminated before the timeout we can break early
-        if (eTaskGetState(http_task_handles[0]) == eDeleted &&
-            eTaskGetState(http_task_handles[1]) == eDeleted &&
-            eTaskGetState(http_task_handles[2]) == eDeleted &&
-            eTaskGetState(http_task_handles[3]) == eDeleted)
-        {
-            break;
-        }
-        vTaskDelay(150 / portTICK_PERIOD_MS);
-    }*/
-
-    // all tasks are now completed. we need to use their vectors
+    handle_tcp_socket(tcp_socket_data_ptr);
 
     // create map from LPSIP and vectors and pass it to the distance estimator
 
-    //std::map<uint16_t, LPSDEVICE(*)[4]> mapped_measurements;
+    std::map<uint16_t, LPSDEVICE *[4]> mapped_measurements;
 
     // todo iterate over each vector
     // if an id is not yet in the map, add it and create the array via MALLOC to store the device ptr at the correct index.
-
-    // mapped_measurements.insert()
+    mapped_measurements.insert({1, (LPSDEVICE *)nullptr});
 }
